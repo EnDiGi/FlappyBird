4a9429343942cc250b4eb389e841205bfd1283ae
EnDiGi <degiorgienrico.26042012@gmail.com>
2024-08-11 20:53:41 +0200

    Update pipe.py

--- pipe.py
+++ pipe.py
@@ -1,14 +1,14 @@
 from random import randint
 
 class Pipe:
-  def __init__(self):
-    self.x = 800
-    self.space = randint(1430 // 10 * 2, 1430 // 10 * 8)
+  def __init__(self, height, width):
+    self.x = width
+    self.space = randint((height - 100) // 10 * 2, (height - 100) // 10 * 8)
   
   def move_and_draw(self, screen, up, down):
-    self.x -= 10
-    self.up_pipe = up.get_rect(topleft=(self.x, (self.space - 210 - 980)))
-    self.down_pipe = down.get_rect(topleft=(self.x, self.space + 210))
+    self.x -= 3
+    self.up_pipe = up.get_rect(topleft=(self.x, (self.space - 100 - 490)))
+    self.down_pipe = down.get_rect(topleft=(self.x, self.space + 100))
     screen.blit(up, self.up_pipe.topleft)
     screen.blit(down, self.down_pipe.topleft)
     
